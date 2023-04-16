66d8a1354d95b56ff417121263465c92ada35891
Davi Roque <sowdavi@gmail.com>
2023-04-15 22:33:04 -0300

    ionic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,8 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'cadastro',
+    loadComponent: () => import('./cadastro/cadastro.page').then( m => m.CadastroPage)
+  },
 ];
---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class CadastroPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
